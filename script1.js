8b4ddc9f6aeaa15fc53ab853ce5e01a847878183
srilakshmi <dirisalasrilakshmi@gmail.com>
2023-02-07 20:31:28 +0530

    added

---
+++ script1.js
@@ -0,0 +1,19 @@
+// var a = 0, b = 1, temp, i;
+
+// for (i = 0; i < 10; i++) {
+//   console.log(a);
+//   temp = a;
+//   a = b;
+//   b = temp + b;
+// }
+// var num = -5;
+
+// if (num >= 0) {
+//   console.log("The number is positive.");
+// } else {
+//   console.log("The number is negative.");
+// }
+var num1 = 5;
+var num2 = 10;
+var result = num1 + num2;
+console.log("The result is: " + result);
